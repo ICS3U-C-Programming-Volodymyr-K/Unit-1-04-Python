a6780bb9fd6235cad666636b7c9f001685fbbd32
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-02-18 17:34:37 +0000

    Created basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,11 @@
+def main():
+    print("1+5={}".format(1 + 5))
+    print("5-2={}".format(5 - 2))
+    print("8*6={}".format(8 * 6))
+    print("5/2={}".format(5 / 2))
+    print("2**3={}".format(2**3))
+    print("8**0.5={}".format(8**0.5))
+
+
+if __name__ == "__main__":
+    main()
